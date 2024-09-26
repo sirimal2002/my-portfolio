47f36da1bfcb0bd586c12f8c69d2853d1a90d8f1
unknown <sirimal2002@gmail.com>
2024-09-27 01:08:28 +0700

    :art: import font-style and do breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px"
+    },
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px"
+      }
+    },
+    fontFamily: {
+      OpenSans: ["Open Sans", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"]
+    }
   },
   plugins: [],
 }
